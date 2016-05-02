8e5deb2b2bd1ed431157d1c6cb318583821d3320
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2016-05-02 13:39:56 -0400

    Mouse flies and falls.
    Added code to make the mouse fly upon left click, and the gravity has
    been adjusted.

--- Jetpack Joyride Tutorial/Assets/Scripts/MouseController.cs
+++ Jetpack Joyride Tutorial/Assets/Scripts/MouseController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class MouseController : MonoBehaviour {
 	public float jetpackForce = 75.0f;
+	private Rigidbody2D myRigidBody2D;
+
 	// Use this for initialization
 	void Start () {
-	
+		myRigidBody2D = this.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -17,9 +19,10 @@ public class MouseController : MonoBehaviour {
 	{
 		bool jetpackActive = Input.GetButton ("Fire1");
 
-		if(jetpackActive)
+		if (jetpackActive) 
 		{
-			
+			myRigidBody2D.AddForce (new Vector2 (0, jetpackForce));
+		}
 			
 	}
 }
